879881daff78edbe79c2b6aaba2a8bd08384366e
minzyee <vvzmsqufvv@gmail.com>
2023-06-21 22:02:52 +0900

    refactor: 랜덤 배경이미지 코드 수정

--- 230619_JS/필수과제-인삿말타이머/js/background.js
+++ 230619_JS/필수과제-인삿말타이머/js/background.js
@@ -1,34 +1,18 @@
 // 배경이미지 랜덤 전환 기능
-// 여러 요소 클릭 시, 선택 되게 만들기
-let imgBox = document.querySelector("#img-box");
+const images = [
+	"assets/img0.jpg",
+	"assets/img1.jpg",
+	"assets/img2.jpg",
+	"assets/img3.jpg",
+	"assets/img4.jpg",
+];
 
-// 이미지들의 배열 --> RANDOM_IMG[0]
-let RANDOM_IMG = ["img0.jpg", "img1.jpg", "img2.jpg", "img3.jpg", "img4.jpg"];
-
-// 화면에 보여줄 이미지
-function printRandomImg() {
-	for (let i = 0; i < RANDOM_IMG.length; i++) {
-		document.getElementById("img-box").innerHTML =
-			"<img src='./assets/" + RANDOM_IMG[i] + "' alt=''>";
-	}
+function getRandomImage() {
+	const randomIndex = Math.floor(Math.random() * images.length);
+	const randomImage = images[randomIndex];
+	document.getElementById("randomImage").src = randomImage;
 }
 
-// 0~7 숫자를 그 안에서 무작위로 정렬하는 함수
-function shuffle(array) {
-	array.sort(() => Math.random() - 0.5);
-}
-
-// 이미지 순서를 셔플 해주는 함수
-function getRandomImg(size) {
-	shuffle(RANDOM_IMG);
-
-	let showImg = [];
-	for (let i = 0; i < RANDOM_IMG.length; i++) {
-		showImg.push(RANDOM_IMG[i]);
-	}
-	return showImg;
-}
-
-// imgList를 출력해서 보여줘야한다.
-const imgList = getRandomImg(RANDOM_IMG.length);
-printRandomImg(imgList);
+document
+	.getElementById("randomImage")
+	.addEventListener("click", getRandomImage);
