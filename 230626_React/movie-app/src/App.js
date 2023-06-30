94422d9b1cf05f4809177c21cb140fd557eed6d5
minzyee <vvzmsqufvv@gmail.com>
2023-07-01 02:10:28 +0900

    feat: 영화 목록 데이터 패칭과 라우터 추가

--- 230626_React/movie-app/src/App.js
+++ 230626_React/movie-app/src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import GlobalStyles from "./GlobalStyles";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navigation from "./components/Navigation";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import MovieList from "./pages/MovieList";
+import MovieDetail from "./pages/MovieDetail";
+import SearchResults from "./pages/SearchResults";
+import { movieDataFetch } from "./services/api";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [movies, setMovies] = useState([]);
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			const moviesData = await movieDataFetch();
+			setMovies(moviesData);
+			setLoading(false);
+		};
+
+		fetchData();
+	}, []);
+
+	return (
+		<>
+			<Router>
+				<Navigation />
+				<Routes>
+					{/* <Route path="/" element={<Home />} /> */}
+					<Route
+						path="/"
+						element={<MovieList movies={movies} loading={loading} />}
+					/>
+					<Route
+						path="/movieDetail/:id"
+						element={<MovieDetail loading={loading} />}
+					/>
+					<Route path="/search" element={<SearchResults />} />
+				</Routes>
+				<Footer />
+			</Router>
+			<GlobalStyles />
+		</>
+	);
 }
 
 export default App;
