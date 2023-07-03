9fcd731281c360fae6efdca8867ac901baeaa75d
minzyee <vvzmsqufvv@gmail.com>
2023-07-03 14:05:21 +0900

    refactor: state 및 fetch 코드 분리

--- 230626_React/movie-app/src/App.js
+++ 230626_React/movie-app/src/App.js
@@ -1,43 +1,19 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import GlobalStyles from "./GlobalStyles";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navigation from "./components/Navigation";
 import Footer from "./components/Footer";
-import Home from "./pages/Home";
-import MovieList from "./pages/MovieList";
-import MovieDetail from "./pages/MovieDetail";
-import SearchResults from "./pages/SearchResults";
-import { movieDataFetch } from "./services/api";
+import Home from "routes/Home";
+import Detail from "routes/Detail";
 
 function App() {
-	const [movies, setMovies] = useState([]);
-	const [loading, setLoading] = useState(true);
-
-	useEffect(() => {
-		const fetchData = async () => {
-			const moviesData = await movieDataFetch();
-			setMovies(moviesData);
-			setLoading(false);
-		};
-
-		fetchData();
-	}, []);
-
 	return (
 		<>
 			<Router>
 				<Navigation />
 				<Routes>
-					{/* <Route path="/" element={<Home />} /> */}
-					<Route
-						path="/"
-						element={<MovieList movies={movies} loading={loading} />}
-					/>
-					<Route
-						path="/movieDetail/:id"
-						element={<MovieDetail loading={loading} />}
-					/>
-					<Route path="/search" element={<SearchResults />} />
+					<Route path="/" element={<Home />} />
+					<Route path="/movie/:id" element={<Detail />} />
 				</Routes>
 				<Footer />
 			</Router>
